024257ba0898113aea5aa813e124801b52e0d1fb
Jacob Gadikian <jacobgadikian@gmail.com>
2021-02-22 16:22:39 +0700

    start on embedding

--- starport/templates/app/new.go
+++ starport/templates/app/new.go
@@ -1,28 +1,32 @@
 package app
 
 import (
+	"embed"
+	_ "embed"
 	"strings"
 
 	"github.com/gobuffalo/genny"
-	"github.com/gobuffalo/packr/v2"
 	"github.com/gobuffalo/plush"
 	"github.com/gobuffalo/plushgen"
 	"github.com/tendermint/starport/starport/pkg/cosmosver"
 )
 
-// these needs to be created in the compiler time, otherwise packr2 won't be
-// able to find boxes.
-var templates = map[cosmosver.MajorVersion]*packr.Box{
-	cosmosver.Launchpad: packr.New("app/templates/launchpad", "./launchpad"),
-	cosmosver.Stargate:  packr.New("app/templates/stargate", "./stargate"),
+//go:embed app/templates/launchpad/*
+var launchpad embed.FS
+
+//go:embed app/templates/stargate/*
+var stargate embed.FS
+
+//Uses embed to embed stargate and Launchpad
+var templates = map[cosmosver.MajorVersion]embed.FS{
+	cosmosver.Launchpad: launchpad,
+	cosmosver.Stargate: stargate,
 }
 
 // New ...
 func New(sdkVersion cosmosver.MajorVersion, opts *Options) (*genny.Generator, error) {
 	g := genny.New()
-	if err := g.Box(templates[sdkVersion]); err != nil {
-		return g, err
-	}
+	if err := g.File(launchpad.ReadDir()
 	ctx := plush.NewContext()
 	ctx.Set("ModulePath", opts.ModulePath)
 	ctx.Set("AppName", opts.AppName)
